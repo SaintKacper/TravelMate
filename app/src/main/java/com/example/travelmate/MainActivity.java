063be97c81fcda8a19516875f0f6317cb49c3353
Lebda-Piotr <piotrlebda12@interia.pl>
2023-11-07 09:04:06 +0100

    przycisk "Lokalizuj mnie" updatuje lokalizację

--- app/src/main/java/com/example/travelmate/MainActivity.java
+++ app/src/main/java/com/example/travelmate/MainActivity.java
@@ -5,10 +5,19 @@ import android.app.Activity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import org.osmdroid.util.GeoPoint;
 import android.widget.LinearLayout;
 import org.osmdroid.api.IMapController;
 import org.osmdroid.views.MapView;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+import android.widget.Toast;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends Activity {
     private MapView mapView;
@@ -16,6 +25,8 @@ public class MainActivity extends Activity {
     private Button locateMeButton;
     private ImageButton compassButton;
     private ImageButton menuButton;
+    private MyLocationNewOverlay myLocationOverlay;
+    private static final int REQUEST_PERMISSIONS_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,13 +49,36 @@ public class MainActivity extends Activity {
         compassButton.setScaleType(ImageButton.ScaleType.FIT_CENTER);
         menuButton.setScaleType(ImageButton.ScaleType.FIT_CENTER);
 
+        // Pobranie aktualnej lokalizacji
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_PERMISSIONS_REQUEST_CODE);
+        } else {
+            myLocationOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(this), mapView);
+            myLocationOverlay.enableMyLocation();
+            mapView.getOverlays().add(myLocationOverlay);
+        }
         locateMeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Tutaj możesz dodać kod do lokalizacji użytkownika.
-                // Możesz użyć Google Maps API lub innych narzędzi do śledzenia lokalizacji.
+                if (myLocationOverlay != null && myLocationOverlay.getMyLocation() != null) {
+                    // Pobranie aktualnej lokalizacji użytkownika
+                    double latitude = myLocationOverlay.getMyLocation().getLatitude();
+                    double longitude = myLocationOverlay.getMyLocation().getLongitude();
+
+                    // Utworzenie obiektu GeoPoint na podstawie lokalizacji użytkownika
+                    org.osmdroid.util.GeoPoint userLocation = new org.osmdroid.util.GeoPoint(latitude, longitude);
+
+
+                    // Ustaw mapę w centrum na podstawie lokalizacji użytkownika
+                    mapController.setCenter(userLocation);
+                } else {
+                    Toast.makeText(MainActivity.this, "Brak dostępu do lokalizacji.", Toast.LENGTH_SHORT).show();
+                }
             }
         });
+
 
         compassButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,5 +93,19 @@ public class MainActivity extends Activity {
                 // Tutaj możesz dodać kod do rozwijania menu i obsługi dodatkowych opcji.
             }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_PERMISSIONS_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                myLocationOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(this), mapView);
+                myLocationOverlay.enableMyLocation();
+                mapView.getOverlays().add(myLocationOverlay);
+            } else {
+                Toast.makeText(this, "Brak uprawnień do lokalizacji.", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
